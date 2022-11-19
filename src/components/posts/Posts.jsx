99c5c3c8be63d5a048b3eb938566c58a4a867f26
FelipeMDantas <felipemdantas@terra.com.br>
2022-11-19 20:23:09 -0300

    Posts and Stories components further developed.

--- src/components/posts/Posts.jsx
+++ src/components/posts/Posts.jsx
@@ -1,7 +1,35 @@
 import "./posts.scss";
+import Post from "../post/Post";
 
 const Posts = () => {
-  return <div className="posts">Posts</div>;
+  //TEMPORARY
+  const posts = [
+    {
+      id: 1,
+      name: "John Doe",
+      userId: 1,
+      profilePic:
+        "https://images.pexels.com/photos/609771/pexels-photo-609771.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit",
+      img: "https://images.pexels.com/photos/12179283/pexels-photo-12179283.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    },
+    {
+      id: 2,
+      name: "Jane Doe",
+      userId: 2,
+      profilePic:
+        "https://images.pexels.com/photos/14239995/pexels-photo-14239995.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+      desc: "Tenetur iste voluptates dolorem rem commodi voluptate pariatur, voluptatum, laboriosam consequatur enim",
+    },
+  ];
+
+  return (
+    <div className="posts">
+      {posts.map((post) => (
+        <Post post={post} key={post.id} />
+      ))}
+    </div>
+  );
 };
 
 export default Posts;
