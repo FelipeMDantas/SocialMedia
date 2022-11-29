8182a39c4ebeb898ee254dae93f554346ad70479
FelipeMDantas <felipemdantas@terra.com.br>
2022-11-28 22:30:12 -0300

    Register and Login requests written; jwt, cors and cookies applied.

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -1,7 +1,9 @@
 import { db } from "../connect.js";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 
 export const register = (req, res) => {
-  const q = "select from users where username = ?";
+  const q = "select * from users where username = ?";
 
   db.query(q, [req.body.username], (err, data) => {
     if (err) return res.status(500).json(err);
@@ -22,9 +24,38 @@ export const register = (req, res) => {
 
     db.query(q, [values], (err, data) => {
       if (err) return res.status(500).json(err);
-      return res.status(200).json("User has been created");
+      return res.status(200).json("User has been created.");
     });
   });
 };
-export const login = (req, res) => {};
-export const logout = (req, res) => {};
+
+export const login = (req, res) => {
+  const q = "select * from users where username = ?";
+
+  db.query(q, [req.body.username], (err, data) => {
+    if (err) return res.status(500).json(err);
+    if (data.length === 0) return res.status(404).json("User not found.");
+
+    const checkPassword = bcrypt.compareSync(
+      req.body.password,
+      data[0].password
+    );
+
+    if (!checkPassword)
+      return res.status(400).json("Wrong password or username!");
+
+    const token = jwt.sign({ id: data[0].id }, "secretkey");
+    const { password, ...rest } = data[0];
+
+    res.cookie("accessToken", token, { httpOnly: true }).status(200).json(rest);
+  });
+};
+export const logout = (req, res) => {
+  res
+    .clearCookie("accessToken", {
+      secure: true,
+      sameSite: "none",
+    })
+    .status(200)
+    .json("User has been logged out.");
+};
